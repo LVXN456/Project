599c96312fd4027cc4d77c13f60f5150340c640a
LVXN456 <xuanbizet08@gmail.com>
2024-08-14 18:36:20 +0200

    test

--- src/fr/lvxn456/monplugin/MonPlugin.java
+++ src/fr/lvxn456/monplugin/MonPlugin.java
@@ -2,7 +2,9 @@ package fr.lvxn456.monplugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MonPlugin extends JavaPlugin {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+
+    @Override
+    public void onEnable() {
+        System.out.println("MonPlugin est allumé");
     }
 }
