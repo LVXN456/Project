0c0862fc68aa5cd8b97114aabbf1c3c8f679f1a7
LVXN456 <xuanbizet08@gmail.com>
2024-08-14 18:14:33 +0200

    test

---
+++ src/fr/lvxn456/monplugin/MonPlugin.java
@@ -0,0 +1,8 @@
+package fr.lvxn456.monplugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MonPlugin extends JavaPlugin {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
